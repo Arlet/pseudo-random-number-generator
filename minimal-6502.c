1bd1c5c6598dd6e95cb2be3a96f6f815f691df48
Arlet Ottens <arlet@ladybug.xs4all.nl>
2023-04-19 10:50:10 +0200

    add some comments to C source

--- minimal-6502.c
+++ minimal-6502.c
@@ -14,28 +14,53 @@
 #include <io.h>
 #endif
 
+/*
+ * ADD without carry. This instruction is not
+ * available on the 6502, but can be implemented
+ * by CLC followed by ADC.  When ADD(p, p) is used
+ * the 6502 can implement that as a 1-bit shift left
+ * (ASL) instruction.
+ * 
+ * The 'x' variable must be declared in the scope
+ * of the caller and will store the carry output
+ * in bit 8.
+ */
 #define ADD( a, b )                 \
     do {                            \
         x = a + (b);                \
         a = x;                      \
     } while (0)
 
+/*
+ * ADD with carry. The expression 'b' is added to
+ * variable 'a' using the carry flag stored in bit 8
+ * of register x.
+ */
 #define ADC( a, b )                 \
     do {                            \
         x = a + (b) + (x >> 8) % 2; \
         a = x;                      \
     } while (0)
 
+/*
+ * main loop, produce random output and send it to stdout
+ */
 int main( void )
 {
-    static char buf[65536];
+    // 5 bytes of state
     uint8_t s0 = 0, s1 = 0, s2 = 0, s3 = 0, s4 = 0;
+
+    // x stores the current carry flag in bit 8
     unsigned int x;
 
+    // use a big output buffer to increase putchar() speed 
+    static char buf[65536];
     setvbuf( stdout, buf, _IOFBF, sizeof(buf) ); 
 #ifdef _WIN32
     _setmode(_fileno(stdout), _O_BINARY);
 #endif
+
+    // random generator loop
     while( 1 )
     {
         ADD( s0, 0x41 );
