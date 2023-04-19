b01d2eed2421c3e1b50459ccefa74c11a1429a2a
Arlet Ottens <arlet@ladybug.xs4all.nl>
2023-04-19 09:59:42 +0200

    Reduced code size & cycle count for slight drop in quality

--- minimal-6502.asm
+++ minimal-6502.asm
@@ -1,25 +1,27 @@
 ;
-; fast PRNG (pseudo random number generator)
-; written in 6502 assembly language
+; fast PRNG (pseudo random number generator) written in 6502 assembly
+; language. Code is optimized for speed and memory footprint, while 
+; still producing good quality random numbers for 6502-based platforms. 
 ;
 ; (C) Arlet Ottens 2023 <arlet@c-scape.nl> 
 ;
-
-; the label 'state' refers to a 6 byte RAM area that must be initialized
+; the label 'state' refers to a 5 byte RAM area that must be initialized
 ; with a random seed value. Any seed value will do, and will result in 
 ; a minimum period of 4G byte.
 ; 
 ; the subroutine returns a random byte in the accumulator
 ;
-; the code takes 46 cycles for the actual update, plus 12 cycles for the 
-; JSR/RTS overhead if used as a subroutine.
+; the code takes 42 cycles for the actual update, plus 12 cycles for the 
+; JSR/RTS overhead if used as a subroutine. The 'state' is assumed to be 
+; in zero page. You can move the state to other memory at the cost of 
+; 12 additional cycles for absolute accesses.
 ;
 ; the output of this random number generator passes Dieharder as well as
-; PractRand up to 4GB output.
+; PractRand up to 2GB output.
 ;
 prng:
     CLC
-    LDA #&45
+    LDA #$41
     ADC state+0
     STA state+0
     ADC state+1
@@ -28,10 +30,9 @@ prng:
     STA state+2
     ADC state+3
     STA state+3
-    EOR state+4
-    ADC state+5
+    ADC state+4
+    ASL
+    ADC state+3
     STA state+4
-    ADC state+5
     EOR state+2
-    STA state+5
     RTS
